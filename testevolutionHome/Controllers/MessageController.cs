4bd0ffff65aee249dd57c7b2b80bcad8760a81a1
YuriGoncharov <YuriGoncharov@DESKTOP-ER3T7LK>
2025-07-08 14:30:59 +0300

    Initial commit

---
+++ testevolutionHome/Controllers/MessageController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace testevolutionHome.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class MessageController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(new { message = "Привет от ASP.NET Core Web API!" });
+        }
+    }
+}
---
+++ testevolutionHome/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace testevolutionHome
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Добавляем поддержку контроллеров
+            builder.Services.AddControllers();
+
+            var app = builder.Build();
+
+            // Используем статические файлы из wwwroot
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
