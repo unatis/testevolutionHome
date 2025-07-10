0491375bee181c0d3f1e3a989ac8cc4f198a0a46
YuriGoncharov <YuriGoncharov@DESKTOP-ER3T7LK>
2025-07-10 14:22:23 +0300

    login popup

---
+++ testevolutionHome/Controllers/AuthController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace testevolutionHome.Controllers
+{
+    public class AuthController : Controller
+    {
+        [HttpPost("/login")]
+        public IActionResult Login(string username, string password)
+        {
+            if (username == "admin" && password == "123")
+            {
+                // Перенаправление на панель
+                return Redirect("/dashboard/index.html");
+            }
+
+            // Можно передавать ошибку через TempData, ViewData и т.п.
+            return Redirect("/index.html");
+        }
+    }
+}
